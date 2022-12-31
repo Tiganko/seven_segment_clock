f155753e0344155426a25972dea4e60ff69d600d
Tiganko <Mohammed.Eltigani.97@gmail.com>
2022-12-31 16:18:39 +0200

    now it should be universal

--- jscript.js
+++ jscript.js
@@ -12,33 +12,34 @@ function telltime(){
 
 	if(h == 0){
 				temp1 = "12:" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "AM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10) + " AM";
 		}else if(h < 10){
 				temp1 = "0" + h + ":" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "AM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10) + " AM";
 		}else if(h < 12){
 				temp1 = h + ":" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "AM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10) + " AM";
 		}else if(h == 12){
 				temp1 = "12:" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "PM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10)+ " PM";
 			}else if(h<22){
 				temp1 = "0" + (h%12) + ":" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "PM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10) + " PM";
 			}else{
 				temp1 = (h%12) + ":" + 
-			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-			 temp2 = "PM, ";
+			((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10) + " PM";
 			}
 	time.innerHTML = temp1;
 	// time.innerHTML = h + ":" + ((m - (m%10))/10) + (m%10) + ":" + ((s - (s%10))/10) + (s%10);
-	temp2 += "GMT+02:00 EEST<hr>";
-	info.innerHTML = temp2;
+	temp1 = now.toTimeString().split(' ');
+	temp2 = temp1[1].substr(0, 6) + ":" + temp1[1].substr(6) + " ";
+	for(i = 2; i < temp1.length; i++){
+		if(temp1[i].includes('('))
+			temp2 += temp1[i].substr(1,1);
+		else
+			temp2 += temp1[i].substr(0,1);
+	}
+	info.innerHTML = temp2 + "<hr>";
 
 	var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -46,7 +47,7 @@ function telltime(){
 	var mm = now.getMonth();
 	var yy = now.getFullYear();
 
-	temp = days[now.getDay()] + ", "+ dd + " " + months[mm] + " " + yy;
+	temp = days[now.getDay()] + ", "+ dd + "-" + months[mm] + "-" + yy;
 	date.innerHTML = temp;
 }
 
